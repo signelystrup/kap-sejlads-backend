b0387c94746179346dcdf57f9856722eb3cc459f
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-06-09 11:56:01 +0200

    init commmit

---
+++ src/main/java/org/example/kapsejladsbackend/KapSejladsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.kapsejladsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapSejladsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KapSejladsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kapsejladsbackend/KapSejladsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kapsejladsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapSejladsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
